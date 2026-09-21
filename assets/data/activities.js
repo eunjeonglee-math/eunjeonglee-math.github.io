7d515ecbb8cbc9e323c0b25c11c4517b097319a3
eunjeonglee-math <eunjeonglee0202@gmail.com>
2026-09-21 15:51:12 +0900

    Add files via upload

--- assets/data/activities.js
+++ assets/data/activities.js
@@ -1071,6 +1071,18 @@ window.ACTIVITY_DATA = {
         label: "2026",
         items: [
           {
+            title: "Dynamics of polynomial vector fields on a sphere",
+            url: "https://math.chungbuk.ac.kr/?mod=view&pg_idx=148&pidx=1830&page=1",
+            lead: "Soumen Sarkar (Indian Institute of Technology Madras), ",
+            details: " · September 18."
+          }
+        ],
+        open: true
+      },
+      {
+        label: "2026",
+        items: [
+          {
             title: "Wave turbulence and some applications",
             url: "https://math.chungbuk.ac.kr/?mod=view&pg_idx=148&pidx=1799&page=1",
             lead: "Joonhyun La (KIAS), ",
